763474710e9b4674d812fadcf108aab6a9c16c31
nicolemah99 <nicolemah99@gmail.com>
2024-10-07 12:19:17 -0800

    Consolidate scss a nd js files, import files in main.js and main.scss

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+/*
+ * This file is part of the theme and overrides the theme's default script file to remove the light theme functionality.
+ * Please do not rename or change the file name, as it is intentionally used to override the theme's dark mode functionality.
+ */
+// Feather Icons
 feather.replace();
 
 const mobileNav = document.getElementById('mobileNav');
