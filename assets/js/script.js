57b2ad9509ed25cbe6bb981eb18e3ee7c3797b3e
nicolemah99 <nicolemah99@gmail.com>
2024-11-05 14:15:03 -0900

    Reintroduce dark theme

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-/*
- * This file is part of the theme and overrides the theme's default script file to remove the light theme functionality.
- * Please do not rename or change the file name, as it is intentionally used to override the theme's dark mode functionality.
- */
-
-
-
